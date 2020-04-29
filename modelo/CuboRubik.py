e6537f8bd90a82a2ac9c70270a511bee2f0521d3
RCrocetti <raf.17.crocetti@gmail.com>
2020-04-28 22:24:01 -0400

    FIX: se agregaron comentarios a gran parte del codigo

--- modelo/CuboRubik.py
+++ modelo/CuboRubik.py
@@ -1,7 +1,9 @@
 import math
 from modelo.SubCubo import SubCubo
+# clase encargada de representar el cubo como un conjunto de subcubos
+# que se mueven dentro de una lista y que seon capaces de cambiar su orientacion
 class CuboRubik:
-    # PENDIENTE: documentar que puta es lo que hice aca
+
     def __init__(self):
         self.SubCubos=[]
 
@@ -115,6 +117,10 @@ class CuboRubik:
                     self.SubCubos.append(subCubo)
                     id=id+1
 
+    # muestra  en plantalla el cubo desarrollado de acuerdo al orden de los plalnos anteriormente mencionado
+    # en vez de colores muestra numeros
+    # colores=["Verde","Amarillo","Rojo","Blanco","Naranja","Azul","Sin Color"]
+    # coloresEnNumeros=[1,2,3,4,5,6,7]
     def print(self):
 
         #cara 0
@@ -190,6 +196,13 @@ class CuboRubik:
         coordenadas=[x,y,z]
         return  coordenadas
 
+    # convierte la posicion de un subCubo en la lista a la posicion de los N planos que le corresponden
+    # a esa posicion siempre que las caras sean la frontal y la trasera
+    # porque se plantea en frente a la cara a rotar y ahi se hace la nomenclatura de plano con las siguientes
+    # reglas:
+    # 1-Siempre se va en sentido horario
+    # 2- siempre se termina en la cara a rotar
+    # 3- se enumera desde el 0 los planos
     def convertirPosicionLinealEnCoordenadasPlanosFrontal(self, lineal):
 
         if lineal == 0:
@@ -231,7 +244,8 @@ class CuboRubik:
         elif lineal == 26:
             planos = [3, 2, 0]
         return planos
-
+    # de forma similar a la anterior pero en este caso las caras a las que se mira de frente son
+    # la izquierda y la derecha, recibira una posicon lineal y devolvera una lista de N planos
     def convertirPosicionLinealEnCoordenadasPlanosIzquierda(self, lineal):
 
         if lineal == 0:
@@ -280,127 +294,104 @@ class CuboRubik:
         elif lineal == 26:
             planos = [ 2, 0,3]
         return planos
-    # convierte la posicion de un subCubo en la lista a la posicion de los N planos que le corresponden
-    # a esa posicion
-    #PENDIENTE: revisar el orden de los planos, debe ser universal
-    # esto funciono perfecto para las frontales y posteriores
-    # PENDIENTE: averiguar por que?
-    def convertirPosicionLinealEnCoordenadasPlanos(self,lineal):
-        
-        if lineal == 0:
-            planos = [ 4, 1 ,5]
-        elif lineal == 1:
-            planos=[4, 5]
-        elif lineal == 2:
-            planos = [3, 4, 5]
-        elif lineal == 3:
-            planos =[4,1]
-        elif lineal == 4:
-            planos =[4]
-        elif lineal == 5:
-            planos =[4,3]
-        elif lineal == 6:
-            planos =[1,4,0]
-        elif lineal == 7:
-            planos =[4,0]
-        elif lineal == 8:
-            planos =[4,3,0]
-        elif lineal == 9:
-            planos =[1,5]
-        elif lineal == 10:
-            planos =[5]
-        elif lineal == 11:
-            planos =[3,5]
-        elif lineal == 12:
-            planos =[1]
-        elif lineal == 13:
-            planos =[6]
-        elif lineal == 14:
-            planos =[3]
-        elif lineal == 15:
-            planos =[1,0]
-        elif lineal == 16:
-            planos =[0]
-        elif lineal == 17:
-            planos =[3,0]
-        elif lineal == 18:
-            planos =[1,2,5]
-        elif lineal == 19:
-            planos =[2,5]
-        elif lineal == 20:
-            planos =[2,3,5]
-        elif lineal == 21:
-            planos =[2,1]
-        elif lineal == 22:
-            planos =[2]
-        elif lineal == 23:
-            planos =[2,3]
-        elif lineal == 24:
-            planos =[2,1,0]
-        elif lineal == 25:
-            planos =[2,0]
-        elif lineal == 26:
-            planos =[3,2,0]
-        return planos
+
+    # rota la cara frontal en sentido horario
     def rotarCaraFrontalHorario(self):
 
-        self.rotarValoresFrontalesReloj([20,18,0,2])
+        self.rotarValoresFrontalesHorario([20,18,0,2])
 
-        self.rotarValoresFrontalesReloj([19,9,1,11])
+        self.rotarValoresFrontalesHorario([19,9,1,11])
 
+    # rota la cara frontal en sentido anti horario
     def rotarCaraFrontalAntiHorario(self):
 
-        self.rotarValoresFrontalesReloj([2,0,18,20])
+        self.rotarValoresFrontalesHorario([2,0,18,20])
 
-        self.rotarValoresFrontalesReloj([11,1,9,19])
+        self.rotarValoresFrontalesHorario([11,1,9,19])
 
+    # rota la cara posterior en sentido horario
     def rotarCaraPosteriorHorario(self):
-        self.rotarValoresFrontalesReloj([8,6,24,26])
 
-        self.rotarValoresFrontalesReloj([17,7,15,25])
+        # usa la misma funcion que frontal porque ambos
+        # utilizan una topologia de planos similar,
+        # como un espejo
+        self.rotarValoresFrontalesHorario([8,6,24,26])
 
+        self.rotarValoresFrontalesHorario([17,7,15,25])
+
+    # rota la cara posterior en sentido anti horario
     def rotarCaraPosteriorAntiHorario(self):
-        self.rotarValoresFrontalesReloj([26,24,6,8])
+        self.rotarValoresFrontalesHorario([26,24,6,8])
 
-        self.rotarValoresFrontalesReloj([25,15,7,17])
+        self.rotarValoresFrontalesHorario([25,15,7,17])
 
+    # rota la cara izquierda en sentido horario
     def rotarCaraIzquierdaHorario(self):
         self.rotarValoresIzquierdaReloj([18, 24, 6, 0])
         self.rotarValoresIzquierdaReloj([21, 15, 3, 9])
 
+    # rota la cara izquierda en sentido anti horario
     def rotarCaraIzquierdaAntiHorario(self):
         self.rotarValoresIzquierdaReloj([18, 0, 6, 24])
         self.rotarValoresIzquierdaReloj([21, 9, 3, 15])
 
+    # rota la cara derecha en sentido horario
     def rotarCaraDerechaHorario(self):
+        # usa la misma funcion que para izquierda porque
+        # tienen un origen de coordenadas comun con esa
+        # defincion de planos
         self.rotarValoresIzquierdaReloj([26,20,2,8])
         self.rotarValoresIzquierdaReloj([23,11,5,17])
 
+    # rota la cara derecha en sentido anti horario
     def rotarCaraDerechaAntiHorario(self):
         self.rotarValoresIzquierdaReloj([26,8,2,20])
         self.rotarValoresIzquierdaReloj([23,17,5,11])
 
-    def rotarValoresFrontalesReloj(self,valoresRotar):
+
+    # toma una lista de posiciones en la lista de subcubos e intercambia en el espacio
+    # de posiciones cartesianas y de orientacion de los subcubos los elementos de la lista
+    # siguiendo el siguiente orden el primer elemnto es sobreescrito por el segundo, el segundo por el tercero
+    # etc
+    def rotarValoresFrontalesHorario(self,valoresRotar):
+        # cantidad de subcuboos a permutar
         cantidadValores=len(valoresRotar)
+
+        # se crea una variable para almacenar la informacion del primer elemento
+        # esto se hace porque no se como declarar metodos estaticos
+        # PENDIENTE: implementarlo con metodos estaticos
         subCuboAuxiliar=SubCubo(0,0,0,0,0)
+        # se guarda una copia del primer elemento
         subCuboAuxiliar.copiarSubCubo(self.SubCubos[valoresRotar[0]])
         for i in range(0,cantidadValores-1,1):
+
+            # posicion actual en la lista de subcubos a permutar
             valorActual=valoresRotar[i]
+            # siguiente posicion en lista de subcubos a permutar
             siguienteValor=valoresRotar[i+1]
 
+            # se transforma al subucubo actual en una copia del siguiente
             self.SubCubos[valorActual].copiarSubCubo(self.SubCubos[siguienteValor])
-            posicionActual=self.convertirPosicionLinealEnCoordenadas(valorActual)
-            self.SubCubos[valorActual].posicionActual=posicionActual
+            # se corrije la posicion para que refleje la que ocupaba el subcubo actual
+            # antes de ser sobre escrito por la del siguiente sub cubo
+            self.SubCubos[valorActual].posicionActual=self.convertirPosicionLinealEnCoordenadas(valorActual)
 
-            planosSiguiente= self.convertirPosicionLinealEnCoordenadasPlanosFrontal(siguienteValor)
+            # lista de planos con colores en la orientacion del subcubo actual
             planosActual= self.convertirPosicionLinealEnCoordenadasPlanosFrontal(valorActual)
+            # lista de planos con colores en la orientacion  del subcubo siguiente
+            planosSiguiente= self.convertirPosicionLinealEnCoordenadasPlanosFrontal(siguienteValor)
+
+            # borra los datos de la orientacion del subcubo actual ya que no solo estan desactualizadas
+            # sino que podria generar errores si no se manda a cero
             self.SubCubos[valorActual].orientacionColoresActual=[0,0,0,0,0,0]
             for j in range(len(planosSiguiente)):
-                planoParaActual=planosActual[j]
-                planoParaSiguiente=planosSiguiente[j]
-                self.SubCubos[valorActual].orientacionColoresActual[planoParaActual]=self.SubCubos[siguienteValor].orientacionColoresActual[planoParaSiguiente]
+                # actualiza los valores de los colores en la orientacion actual para que se
+                # matcheen con los del subocubo siguiente
+                self.SubCubos[valorActual].orientacionColoresActual[planosActual[j]]=self.SubCubos[siguienteValor].orientacionColoresActual[planosSiguiente[j]]
 
 
+        # repite el proceso con el ultimo subcubo asociandole los datos del priemro que se
+        # guardaron anteriormente
         valorActual=valoresRotar[len(valoresRotar)-1]
         siguienteValor=valoresRotar[0]
         self.SubCubos[valorActual].copiarSubCubo(subCuboAuxiliar)
@@ -413,7 +404,10 @@ class CuboRubik:
                 planoParaSiguiente=planosSiguiente[j]
                 self.SubCubos[valorActual].orientacionColoresActual[planoParaActual]=subCuboAuxiliar.orientacionColoresActual[planoParaSiguiente]
 
-
+    # actua de forma identica que el metodo anterior pero las definiciones de los
+    # planos que consulta son distintas , es decir corresponden a otra funcion de matcheo del espacio de la recta
+    # dentro del array de subcubos y un conjunto de 1 a 3 planos ordenados que indican las caras que este subcubo tiene
+    # pintadas
     def rotarValoresIzquierdaReloj(self,valoresRotar):
         cantidadValores=len(valoresRotar)
         subCuboAuxiliar=SubCubo(0,0,0,0,0)
