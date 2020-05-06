285af6b418d9f2bbeb80224771e8b03e5511b906
Rafael Crocetti <raf.17.crocetti@gmail.com>
2020-05-05 23:05:50 -0400

    FEAT: se crearon las funciones obtenerListaSolucion, invertirLista y dibujarSolucion

--- vista/__init__.py
+++ vista/__init__.py
@@ -1,6 +1,6 @@
 from modelo.CuboRubik import CuboRubik
 from modelo.Nodo import Nodo
-
+import copy
 
 def buscarSolucion(nodoInicio,cola,nodosMuertos):
     nodoActual = nodoInicio
@@ -13,6 +13,11 @@ def buscarSolucion(nodoInicio,cola,nodosMuertos):
         for nodo in nodosHijos:
             if nodoVivo(nodosMuertos,nodo):
                 insertarNodo(cola,nodo)
+        # el oredenador de la lista esta funcionando para 12 elementos
+        # PENDIENTE: el error puedede estar en la generacion de la distancia al origen o
+        # la distancia al objetivo(estos dos ultimos comprenden implicitamente una revision del metodo de medicion de distancia)
+        # o en el reemplazo de variables en el proceso de busqueda
+        # de la solucion
         ordenarLista(cola)
     return nodoActual
 
@@ -49,11 +54,42 @@ def ordenarLista(cola):
                 cola[i] = cola[i + 1]
                 cola[i + 1] = temp
 
+# obtiene la lista de movimientos desde el nodo inicial hasta la solucion
+def obtenerListaSolucion(nodo):
+    padre = nodo.nodoPadre
+    nodoActual = nodo
+    movimientos=[]
+    while padre != None:
+        movimientos.append(nodoActual.movimientoGenerador)
+        nodoActual = nodoActual.nodoPadre
+        padre = nodoActual.nodoPadre
+    return invertirLista(movimientos)
+
+# toma una lista y la invierte haciendo del primer elemento el ultimo
+def invertirLista(lista):
+    listaNueva=[]
+    for i in range(len(lista)-1,-1,-1):
+        listaNueva.append(lista[i])
+    return  listaNueva
+
+# dibuja la solucion desde el nodo inicial hasta el final
+# indicando el movimiento realizado a cada paso
+def dibujarSolucion(nodoInicial,nodoFinal):
+    # se hace esta se hace para no modificar al cubo del nodo inicial
+    # cada vez que se corra este metodo
+    cuboRevuelto=copy.deepcopy(nodoInicial.cuboRubik)
+    listaMovimientos=obtenerListaSolucion(nodoFinal)
+    cuboRevuelto.print()
+    for movimiento in listaMovimientos:
+        cuboRevuelto.realizarMovimiento(movimiento)
+        print("Movimiento :"+movimiento)
+        cuboRevuelto.print()
+
+
 
 if __name__ == '__main__':
     cubo=CuboRubik()
-    cubo.realizarMovimiento(cubo.generarSecuenciaMezclado(3))
-
+    cubo.realizarMovimiento(cubo.generarSecuenciaMezclado(2))
     nodo=Nodo(None,None,cubo)
     nodoInicio = nodo
     cola = []
@@ -61,11 +97,11 @@ if __name__ == '__main__':
     nodoInicio.distanciaOrigen = 0
     cola.append(nodoInicio)
 
+    # PENDIENTE: algo esta haciendo mal el buscador de soluciones
+    # solucionarlo
     solucion=buscarSolucion(nodoInicio,cola,nodosMuertos)
-    padre=solucion.nodoPadre
-    nodoActual=solucion
-    while padre!=None:
-        nodoActual.cuboRubik.print()
-        print("")
-        nodoActual=nodoActual.nodoPadre
-        padre=nodoActual.nodoPadre
+
+    # PENDIENTE: probar el metodo de medicion de distancia en cubos
+    # para dos movimientos al azar la distancia entre el inicio y el final debe ser entre
+    # 16 y 32 medido en manhatam
+    dibujarSolucion(nodoInicio,solucion)
